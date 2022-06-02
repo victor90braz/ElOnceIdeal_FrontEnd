470256e805d52ecb26e8cbd671654770e6242739
victor90braz <victor.90braz@gmail.com>
2022-06-02 02:43:32 +0200

    Created registerFormComponent and tested it

--- src/components/RegisterForm/RegisterForm.test.js
+++ src/components/RegisterForm/RegisterForm.test.js
@@ -1,6 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
+
 import store from "../../redux/store/store";
 
 import RegisterForm from "./RegisterForm";
