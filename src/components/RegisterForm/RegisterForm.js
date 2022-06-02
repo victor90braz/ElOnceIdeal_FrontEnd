101655ccdea17491b60c2dc6d2893d0535d2329c
victor90braz <victor.90braz@gmail.com>
2022-06-02 23:02:56 +0200

    Added LoginForm to render in the App

--- src/components/RegisterForm/RegisterForm.js
+++ src/components/RegisterForm/RegisterForm.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { registerThunk } from "../../redux/thunks/userThunks";
+
 import RegisterFormStyle from "./RegisterFormStyle";
 
 const RegisterForm = () => {
