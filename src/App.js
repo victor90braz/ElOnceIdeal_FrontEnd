829aab34a9adb04c38d4ccf2ca3cd0dfa92220b1
victor90braz <victor.90braz@gmail.com>
2022-06-02 09:18:15 +0200

    Styled register form

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import RegisterForm from "./components/RegisterForm/RegisterForm";
 
 const AppStyle = styled.div`
   display: flex;
@@ -11,6 +12,7 @@ function App() {
     <>
       <AppStyle>
         <h1>El Once Ideal</h1>
+        <RegisterForm />
       </AppStyle>
     </>
   );
