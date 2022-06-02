8467f962f045a923ba7ab6518dd16d2c9d354aa0
victor90braz <victor.90braz@gmail.com>
2022-06-02 13:02:26 +0200

    Removed useEffect and tested post method

--- src/components/RegisterForm/RegisterForm.test.js
+++ src/components/RegisterForm/RegisterForm.test.js
@@ -9,7 +9,9 @@ import RegisterForm from "./RegisterForm";
 describe("Given a RegisterForm component", () => {
   describe("When the word 'hello' is written to the username input field", () => {
     test("Then the value of the username input field should be 'hello'", () => {
-      const labelToFind = "Name";
+      const labelName = "Name";
+      const labelSurname = "Username";
+      const labelPassowrd = "Password";
 
       render(
         <Provider store={store}>
@@ -19,9 +21,31 @@ describe("Given a RegisterForm component", () => {
         </Provider>
       );
 
-      const label = screen.getByLabelText(labelToFind);
+      const label1 = screen.getByLabelText(labelName);
+      const label2 = screen.getByLabelText(labelSurname);
+      const label3 = screen.getByLabelText(labelPassowrd);
 
-      expect(label).toBeInTheDocument();
+      expect(label1).toBeInTheDocument();
+      expect(label2).toBeInTheDocument();
+      expect(label3).toBeInTheDocument();
+    });
+  });
+
+  describe("When the word 'Register' is written to the header element", () => {
+    test("Then the value of the header element should be 'Register'", () => {
+      const expectedResult = "Register";
+
+      render(
+        <Provider store={store}>
+          <BrowserRouter>
+            <RegisterForm />
+          </BrowserRouter>
+        </Provider>
+      );
+
+      const receivedResult = screen.getByText(expectedResult);
+
+      expect(receivedResult).toBeInTheDocument();
     });
   });
 });
