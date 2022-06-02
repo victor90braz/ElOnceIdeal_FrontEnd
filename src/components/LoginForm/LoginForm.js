b0b29b5e9811530c9f9b762d641b9c172df0ec75
victor90braz <victor.90braz@gmail.com>
2022-06-02 22:48:40 +0200

    Created LoginForm component and tested it

---
+++ src/components/LoginForm/LoginForm.js
@@ -0,0 +1,58 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { loginThunk } from "../../redux/thunks/userThunks";
+
+import LoginFormStyle from "./LoginFormStyle";
+
+const LoginForm = () => {
+  const dispatch = useDispatch();
+
+  const initialFormValue = {
+    username: "",
+    password: "",
+  };
+
+  const [formValues, setFormValues] = useState(initialFormValue);
+
+  const handleInputChange = (event) => {
+    setFormValues({ ...formValues, [event.target.id]: event.target.value });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (formValues.username === "" || formValues.password === "") {
+      return;
+    }
+    dispatch(loginThunk(formValues));
+    setFormValues(initialFormValue);
+  };
+
+  return (
+    <LoginFormStyle>
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit} noValidate>
+        <label htmlFor="username">Username</label>
+        <input
+          type="text"
+          id="username"
+          value={formValues.username}
+          onChange={handleInputChange}
+          autoComplete="off"
+        />
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          id="password"
+          value={formValues.password}
+          onChange={handleInputChange}
+          autoComplete="off"
+        />
+        <button className="form-button" type="submit">
+          Login
+        </button>
+      </form>
+    </LoginFormStyle>
+  );
+};
+
+export default LoginForm;
---
+++ src/components/LoginForm/LoginForm.test.js
@@ -0,0 +1,81 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import store from "../../redux/store/store";
+
+import LoginForm from "./LoginForm";
+
+describe("Given a LoginForm component", () => {
+  describe("When the word 'hello' is written to the username input field", () => {
+    test("Then the value of the username input field should be 'hello'", () => {
+      const labelToFind = "Username";
+      const inputText = "hello";
+
+      render(
+        <Provider store={store}>
+          <BrowserRouter>
+            <LoginForm />
+          </BrowserRouter>
+        </Provider>
+      );
+
+      const label = screen.getByLabelText(labelToFind);
+      userEvent.type(label, inputText);
+
+      expect(label).toHaveValue(inputText);
+    });
+  });
+
+  describe("When the two inputs have text and the submit button is clicked", () => {
+    test("Then the two inputs should be empty", () => {
+      const usernameLabel = "Username";
+      const passwordLabel = "Password";
+      const inputText = "hello";
+
+      render(
+        <Provider store={store}>
+          <BrowserRouter>
+            <LoginForm />
+          </BrowserRouter>
+        </Provider>
+      );
+
+      const username = screen.getByLabelText(usernameLabel);
+      const password = screen.getByLabelText(passwordLabel);
+      const submitButton = screen.getByRole("button");
+
+      userEvent.type(username, inputText);
+      userEvent.type(password, inputText);
+      userEvent.click(submitButton);
+
+      expect(username).toHaveValue("");
+      expect(password).toHaveValue("");
+    });
+  });
+
+  describe("When the username is 'hello', the password input is empty and the submit is clicked", () => {
+    test("Then the username should be 'hello'", () => {
+      const usernameLabel = "Username";
+      const inputText = "hello";
+
+      render(
+        <Provider store={store}>
+          <BrowserRouter>
+            <LoginForm />
+          </BrowserRouter>
+        </Provider>
+      );
+
+      const username = screen.getByLabelText(usernameLabel);
+
+      const submitButton = screen.getByRole("button");
+
+      userEvent.type(username, inputText);
+
+      userEvent.click(submitButton);
+
+      expect(username).toHaveValue(inputText);
+    });
+  });
+});
