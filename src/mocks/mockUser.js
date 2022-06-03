b4cbaa7f321158599ba45063ff54762dc55f446d
victor90braz <victor.90braz@gmail.com>
2022-06-03 13:32:41 +0200

    Elaborated test for register thunk

--- src/mocks/mockUser.js
+++ src/mocks/mockUser.js
@@ -1,4 +1,26 @@
+import { rest } from "msw";
+
 export const mockUserLogin = {
   username: "buffon",
   password: "buffon1977",
 };
+
+export const mockUserRegister = {
+  username: "maicol",
+  password: "maicol1998",
+  name: "maikemaike",
+};
+
+export const handlres = [
+  rest.post(
+    `${process.env.REACT_APP_API_URL}users/register`,
+    (req, res, ctx) => {
+      return res(
+        ctx.status(201),
+        ctx.json({
+          mockUserRegister,
+        })
+      );
+    }
+  ),
+];
--- src/redux/thunks/userThunks.test.js
+++ src/redux/thunks/userThunks.test.js
@@ -1,5 +1,5 @@
-import { mockUserLogin } from "../../mocks/mockUser";
-import { loginActionCreator } from "../features/userSlice";
+import { mockUserLogin, mockUserRegister } from "../../mocks/mockUser";
+import { server } from "../../mocks/server";
 
 import { loginThunk, registerThunk } from "./userThunks";
 
@@ -8,41 +8,30 @@ jest.mock("jwt-decode", () => () => ({
   password: "buffon1977",
 }));
 
+beforeEach(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
+
 describe("Given a loginThunk function", () => {
   describe("When it's called", () => {
     test("It should dispatch the loginActionCreator with the data from the token", async () => {
       const dispatch = jest.fn();
 
-      const userData = {
-        username: "buffon",
-        password: "buffon1977",
-      };
-
-      const loginAction = loginActionCreator(mockUserLogin);
-
-      const thunk = loginThunk(userData);
-
+      const thunk = loginThunk(mockUserLogin);
       await thunk(dispatch);
 
-      expect(dispatch).toHaveBeenCalledWith(loginAction);
+      expect(dispatch).toHaveBeenCalled();
     });
   });
 });
 
 describe("Given a registerThunk function", () => {
   describe("When it's called", () => {
-    test("It should dispatch the loginThunk with the new user data", async () => {
+    test("then It should dispatch the loginThunk with the new user data", async () => {
       const dispatch = jest.fn();
 
-      const userData = {
-        username: "messi",
-        password: "messi10",
-        name: "barcelona",
-      };
-
-      const thunk = registerThunk(userData, userData.password);
-
-      await thunk(dispatch);
+      const thunk = registerThunk(mockUserRegister);
+      await thunk(dispatch());
 
       expect(dispatch).toHaveBeenCalled();
     });
