6f192d1ca5223324fd0ebca09060d01fa57d8160
victor90braz <victor.90braz@gmail.com>
2022-06-03 15:48:25 +0200

    Changed process.env in the handlres

--- src/mocks/mockUser.js
+++ src/mocks/mockUser.js
@@ -12,15 +12,12 @@ export const mockUserRegister = {
 };
 
 export const handlres = [
-  rest.post(
-    `${process.env.REACT_APP_API_URL}users/register`,
-    (req, res, ctx) => {
-      return res(
-        ctx.status(201),
-        ctx.json({
-          mockUserRegister,
-        })
-      );
-    }
-  ),
+  rest.post(`${process.env.REACT_APP_API_URL}users/login`, (req, res, ctx) => {
+    return res(
+      ctx.status(201),
+      ctx.json({
+        mockUserRegister,
+      })
+    );
+  }),
 ];
