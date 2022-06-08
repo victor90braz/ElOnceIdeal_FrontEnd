515d8999b22c76bd9198d1434e9c344674fe50ec
victor90braz <victor.90braz@gmail.com>
2022-06-08 20:42:40 +0200

    Created deleteThunk and tested it

--- src/redux/thunks/playersThunks.js
+++ src/redux/thunks/playersThunks.js
@@ -1,5 +1,9 @@
 import axios from "axios";
-import { loadAllPlayersActionCreator } from "../features/playerSlice";
+import { correctAction } from "../../components/modals/modals";
+import {
+  deletePlayerActionCreator,
+  loadAllPlayersActionCreator,
+} from "../features/playerSlice";
 
 const url = process.env.REACT_APP_API_URL;
 
@@ -13,4 +17,22 @@ export const getPlayerThunk = (idPlayer) => async (dispatch) => {
   const { data: player } = await axios.get(`${url}players/${idPlayer}`);
 
   dispatch(loadAllPlayersActionCreator(player));
+};
+
+export const deletePlayerThunk = (id) => async (dispatch) => {
+  const token = localStorage.getItem("token");
+
+  const { status } = await axios.delete(
+    `${process.env.REACT_APP_API_URL}players/${id}`,
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  );
+
+  if (status === 200) {
+    dispatch(deletePlayerActionCreator(id));
+    correctAction("Player has been deleted correctly");
+  }
 };
--- src/redux/thunks/playersThunks.test.js
+++ src/redux/thunks/playersThunks.test.js
@@ -1,7 +1,15 @@
 import "../../mocks/server";
 import { mockPlayer, mockPlayers } from "../../mocks/playerMock";
-import { loadAllPlayersActionCreator } from "../features/playerSlice";
-import { getPlayerThunk, loadPlayersThunk } from "./playersThunks";
+import {
+  deletePlayerActionCreator,
+  loadAllPlayersActionCreator,
+} from "../features/playerSlice";
+import {
+  deletePlayerThunk,
+  getPlayerThunk,
+  loadPlayersThunk,
+} from "./playersThunks";
+import axios from "axios";
 
 describe("Given the loadPlayersThunk function", () => {
   describe("When invoked", () => {
@@ -28,6 +36,39 @@ describe("Given the getGameThunk function", () => {
       await thunk(dispatch);
 
       expect(dispatch).toHaveBeenCalledWith(expectedAction);
+    });
+  });
+});
+
+describe("Given the deletePlayerThunk function", () => {
+  describe("When it's called with an id", () => {
+    test("Then it should call dispatch with the loadPlayer action with the Player received from the axios request", async () => {
+      const id = "2";
+      const dispatch = jest.fn();
+      const action = deletePlayerActionCreator(id);
+
+      jest.spyOn(Storage.prototype, "getItem").mockReturnValue("token");
+      axios.delete = jest.fn().mockResolvedValue({ status: 200 });
+
+      const thunk = deletePlayerThunk(id);
+      await thunk(dispatch);
+
+      expect(dispatch).toHaveBeenCalledWith(action);
+    });
+  });
+
+  describe("When it's called with an existent id", () => {
+    test("Then it should not call the dispatch", async () => {
+      const id = "2";
+      const dispatch = jest.fn();
+
+      jest.spyOn(Storage.prototype, "getItem").mockReturnValue("token");
+      axios.delete = jest.fn().mockResolvedValue({});
+
+      const thunk = deletePlayerThunk(id);
+      await thunk(dispatch);
+
+      expect(dispatch).not.toHaveBeenCalled();
     });
   });
 });
