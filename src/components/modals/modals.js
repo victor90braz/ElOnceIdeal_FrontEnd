b559239623c9b588e893c63cd4b182ece5dc2976
victor90braz <victor.90braz@gmail.com>
2022-06-08 21:10:45 +0200

    Dispatched thunk delete function on Player Component

--- src/components/modals/modals.js
+++ src/components/modals/modals.js
@@ -3,10 +3,13 @@ import { toast } from "react-toastify";
 let idLoadingModal = import("react-toastify").Id;
 
 export const correctAction = (message) => toast.success(message);
+
 export const wrongAction = (message) => toast.error(message);
+
 export const setLoadingOn = (message) => {
   idLoadingModal = toast.loading(message);
 };
+
 export const setLoadingOff = () => toast.dismiss(idLoadingModal);
 
 export const registered = () =>
