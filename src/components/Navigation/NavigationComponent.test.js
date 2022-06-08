127e6537db53c181f17f049d4a127e60a3aa36ee
victor90braz <victor.90braz@gmail.com>
2022-06-08 22:59:04 +0200

    Added modal messages when you logged out

--- src/components/Navigation/NavigationComponent.test.js
+++ src/components/Navigation/NavigationComponent.test.js
@@ -1,14 +1,18 @@
 import { render, screen } from "@testing-library/react";
+import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
+import store from "../../redux/store/store";
 import NavigationComponent from "./NavigationComponent";
 
 describe("Given a NavigationComponent", () => {
   describe("When its called to be rendered", () => {
     test("Then it should create a NavigationComponent with four list components", () => {
       render(
-        <BrowserRouter>
-          <NavigationComponent />
-        </BrowserRouter>
+        <Provider store={store}>
+          <BrowserRouter>
+            <NavigationComponent />
+          </BrowserRouter>
+        </Provider>
       );
 
       const displayHeader = screen.getAllByRole("listitem");
