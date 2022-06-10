61001bf3ae12ec72b73ace6dcdb2fe881175a0b5
victor90braz <victor.90braz@gmail.com>
2022-06-10 21:05:58 +0200

    Created edit slice reducer and tested it

--- src/redux/features/playerSlice.test.js
+++ src/redux/features/playerSlice.test.js
@@ -1,98 +1,91 @@
-import playerReducer, {
+import { mockPlayer, mockPlayers } from "../../mocks/playerMock";
+import {
   deletePlayerActionCreator,
+  editPlayerActionCreator,
   loadAllPlayersActionCreator,
 } from "./playerSlice";
 
-describe("Given a playerSlice reducer", () => {
+import playerReducer from "../features/playerSlice";
+
+describe("Given a playerReducer reducer", () => {
   describe("When its invoked with an unknown action and a player as initialValue", () => {
-    test("Then it should return the same player", () => {
+    test("Then it should return the same initial value", () => {
       const action = {
-        type: "player/unknownAction",
+        type: "user/unknownAction",
       };
 
-      const initialValue = [
-        {
-          name: "Buffon",
-          image:
-            "https://www.futbin.com/content/fifa22/img/players/p50332827.png?v=15",
-          username: "buffon1977",
-        },
-      ];
+      const initialState = {
+        allPlayers: mockPlayers,
+      };
 
-      const receivedValue = playerReducer(initialValue, action);
+      const receivedValue = playerReducer(initialState, action);
 
-      expect(receivedValue).toBe(initialValue);
+      expect(receivedValue).toEqual(initialState);
     });
   });
 
   describe("When its invoked with a player action and a user", () => {
     test("Then it should return the user", () => {
-      const player = [
-        {
-          name: "Buffon",
-          image:
-            "https://www.futbin.com/content/fifa22/img/players/p50332827.png?v=15",
-          username: "buffon1977",
-        },
-      ];
-
-      const loadedPlayer = {
-        allPlayers: [
-          {
-            name: "Buffon",
-            image:
-              "https://www.futbin.com/content/fifa22/img/players/p50332827.png?v=15",
-            username: "buffon1977",
-          },
-        ],
-      };
-
-      const action = {
-        type: "player/unknownAction",
-        allPlayers: loadAllPlayersActionCreator(player),
-      };
+      const action = loadAllPlayersActionCreator(mockPlayer);
 
       const initialState = {
-        allPlayers: [
-          {
-            image:
-              "https://www.futbin.com/content/fifa22/img/players/p50332827.png?v=15",
-            name: "Buffon",
-            username: "buffon1977",
-          },
-        ],
+        allPlayers: mockPlayers,
       };
-      const receivedValue = playerReducer(initialState, action);
 
-      expect(receivedValue).toStrictEqual(loadedPlayer);
+      const { allPlayers } = playerReducer(initialState, action);
+
+      expect(allPlayers).toEqual(mockPlayer);
     });
   });
-});
 
-describe("Given a delete reducer", () => {
-  describe("When its invoked with an unknown action and a player as initialValue", () => {
-    test("Then it should delete the player", () => {
-      const initialValue = {
-        allPlayers: [
-          {
-            id: "1",
-            name: "Ronaldo",
-            username: "CR7",
-            perfil: "Portugal",
-          },
-        ],
+  describe("When its invoked with an deletePlayer action with an id and a initial state with one player", () => {
+    test("Then it should return one note at allNotes and at notesToShow", () => {
+      const expectedLength = 1;
+
+      const action = deletePlayerActionCreator(mockPlayers[0].id);
+
+      const initialState = {
+        allPlayers: mockPlayers,
       };
 
-      const expectedState = {
-        allPlayers: [],
+      const { allPlayers } = playerReducer(initialState, action);
+
+      expect(allPlayers).toHaveLength(expectedLength);
+    });
+  });
+
+  describe("When its invoked with a edit action with one player as a initial state and a edited player", () => {
+    test("Then it should edit the player and change the properties", () => {
+      const editedPlayer = {
+        image:
+          "https://www.futbin.com/content/fifa22/img/players/p50332827.png?v=16",
+        name: "Neymar",
+        position: "delantero",
+        speed: "99",
+        shoot: "90",
+        pass: "95",
+        agility: "90",
+        defense: "45",
+        strength: "80",
       };
 
-      const id = "1";
+      const action = editPlayerActionCreator(editedPlayer);
 
-      const action = deletePlayerActionCreator(id);
-      const newState = playerReducer(initialValue, action);
+      const initialState = {
+        allPlayers: mockPlayer,
+      };
 
-      expect(newState).toStrictEqual(expectedState);
+      const { allPlayers } = playerReducer(initialState, action);
+
+      expect(allPlayers[0]).toHaveProperty("image", editedPlayer.image);
+      expect(allPlayers[0]).toHaveProperty("name", editedPlayer.name);
+      expect(allPlayers[0]).toHaveProperty("position", editedPlayer.position);
+      expect(allPlayers[0]).toHaveProperty("speed", editedPlayer.speed);
+      expect(allPlayers[0]).toHaveProperty("shoot", editedPlayer.shoot);
+      expect(allPlayers[0]).toHaveProperty("pass", editedPlayer.pass);
+      expect(allPlayers[0]).toHaveProperty("agility", editedPlayer.agility);
+      expect(allPlayers[0]).toHaveProperty("defense", editedPlayer.defense);
+      expect(allPlayers[0]).toHaveProperty("strength", editedPlayer.strength);
     });
   });
 });
