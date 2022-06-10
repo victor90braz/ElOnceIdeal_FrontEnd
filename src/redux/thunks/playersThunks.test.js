ac034db9017a49af143fc40f9516c1ede809f42a
victor90braz <victor.90braz@gmail.com>
2022-06-10 21:22:38 +0200

    Created edit and tested it

--- src/redux/thunks/playersThunks.test.js
+++ src/redux/thunks/playersThunks.test.js
@@ -2,10 +2,12 @@ import "../../mocks/server";
 import { mockPlayer, mockPlayers } from "../../mocks/playerMock";
 import {
   deletePlayerActionCreator,
+  editPlayerActionCreator,
   loadAllPlayersActionCreator,
 } from "../features/playerSlice";
 import {
   deletePlayerThunk,
+  editPlayerThunk,
   getPlayerThunk,
   loadPlayersThunk,
 } from "./playersThunks";
@@ -66,6 +68,36 @@ describe("Given the deletePlayerThunk function", () => {
       axios.delete = jest.fn().mockResolvedValue({});
 
       const thunk = deletePlayerThunk(id);
+      await thunk(dispatch);
+
+      expect(dispatch).not.toHaveBeenCalled();
+    });
+  });
+});
+
+describe("Given the editPlayerThunk function", () => {
+  describe("When it's called with an id to edit and a player", () => {
+    test("Then it should call dispatch with the new player edited received from the axios request", async () => {
+      const dispatch = jest.fn();
+      const action = editPlayerActionCreator(mockPlayers);
+
+      jest.spyOn(Storage.prototype, "getItem").mockReturnValue("token");
+      axios.put = jest.fn().mockResolvedValue({ data: mockPlayers });
+
+      const thunk = editPlayerThunk(mockPlayers.id, mockPlayers);
+      await thunk(dispatch);
+
+      expect(dispatch).toHaveBeenCalledWith(action);
+    });
+  });
+
+  describe("When it's called and there is no token", () => {
+    test("Then it should not call dispatch", async () => {
+      const dispatch = jest.fn();
+
+      jest.spyOn(Storage.prototype, "getItem").mockReturnValue("");
+
+      const thunk = editPlayerThunk(mockPlayers.id, mockPlayers);
       await thunk(dispatch);
 
       expect(dispatch).not.toHaveBeenCalled();
